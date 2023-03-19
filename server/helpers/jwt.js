ac378ba11c1c951b3828ec5b339747bf3e931059
xaunikmas <xaunikmas@gmail.com>
2023-03-19 11:02:33 +0700

    nambah

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -1,1 +1,16 @@
 const jwt = require ("jsonwebtoken");
+
+const secretCode = process.env.SECRET_CODE || "bebas";
+
+const tokenGenerator = (data) => {
+const {id,username,email,image} = data;
+return jwt.sign ({
+    id,username,email,image
+}, secretCode)
+}
+
+const tokenVerifier = (data) => {
+    return jwt.verify(data,secretCode)
+}
+
+module.exports = {tokenGenerator, tokenVerifier};
